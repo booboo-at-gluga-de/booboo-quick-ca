1f171032f880ab42194e5d3b082db48ea2297cdc
Bernd Stroessenreuther <booboo@gluga.de>
2017-05-19 15:00:32 +0200

    adding GPL headers in any script

--- bin/setup_CA.sh
+++ bin/setup_CA.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# BooBoo Quick CA
+# Copyright (C) 2017, Bernd Stroessenreuther <booboo@gluga.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # 2017-03-22 booboo:
 # this script initializes the CA with all needed config files
 # and initially creates the CA (root) certificate
--- bin/sign_customer_cert.sh
+++ bin/sign_customer_cert.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# BooBoo Quick CA
+# Copyright (C) 2017, Bernd Stroessenreuther <booboo@gluga.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # 2017-05-15 booboo:
 # This script signs a CSR (Certificate Signing Request) for a CA customer
 # and creates a customer certifcate.
